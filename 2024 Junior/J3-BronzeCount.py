38bbc8990f8c495097bd87964fbc87badd1876d2
devnix <arch_devnix@proton.me>
2026-09-24 07:17:37 -0400

    Improve score processing in J3-BronzeCount.py
    Refactor score sorting and duplicate removal for clarity.

--- 2024 Junior/J3-BronzeCount.py
+++ 2024 Junior/J3-BronzeCount.py
@@ -3,18 +3,17 @@ def function():
     inputs = sys.stdin.read().split()
 
     if len(inputs) < 3:
-
         return
 
     N = int(inputs[0]) # the amount of scores
 
-    index = 1
-
     score = [int(i) for i in inputs[1:N+1]]
-    sorted_scores = sorted(score, reverse=True)
-    unique_sorted_scores = list(dict.fromkeys(sorted_scores))
+    sorted_scores = sorted(score, reverse=True) # sorts the scores from max to min
+    unique_sorted_scores = list(dict.fromkeys(sorted_scores)) # remove duplicates
     third_place = unique_sorted_scores[2]
     
     scores = [i for i in sorted_scores if i == third_place]
 
     print(f'{third_place} {len(scores)}')
+
+# O(N log N)
